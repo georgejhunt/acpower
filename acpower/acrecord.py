11bf2f548f039863250283bc3b4d276362d12629
George Hunt <georgejhunt@gmail.com>
2015-02-02 17:56:04 -0500

    cloud needs to start vpn

--- acpower/acrecord.py
+++ acpower/acrecord.py
@@ -490,8 +490,7 @@ class ShowPowerHistory(Tools):
                         else:
                             sys.stdout.write(" ")
                     print
-
-            print "\n0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23"
+                    print "\n0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23"
 
             if args.powersegments:
                 print("\nINDIVIDUAL POWER PERIODS:")
